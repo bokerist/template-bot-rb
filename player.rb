8070cf8a703b9f2e93a4133c09ef8dce450ea4b9
Bruno Scopelliti <bru.scopelliti@gmail.com>
2019-12-28 08:44:43 +0100

    Move logger as local var

--- player.rb
+++ player.rb
@@ -1,14 +1,13 @@
 require "logger"
 
-logger = Logger.new(STDOUT)
-logger.level = Logger::INFO
-
 class Player
   def getVersion ()
     "Default rb-player"
   end
 
   def bet (gamestate)
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::INFO
     logger.info("Playing game #{gamestate.tournamentId}")
     0
   end
