23a9982dd1abc3224f1b5d42fed482fa62035d11
Bruno Scopelliti <bru.scopelliti@gmail.com>
2019-12-29 07:19:28 +0100

    Add inline doc

--- player.rb
+++ player.rb
@@ -1,10 +1,14 @@
 require "logger"
 
 class Player
+  # Returns the current version of the deployed bot.
+  # Useful to get debug information.
   def getVersion ()
     "Default rb-player"
   end
 
+  # Returns the amount for the next bet.
+  # Return 0 to fold.
   def bet (gamestate)
     logger = Logger.new(STDOUT)
     logger.level = Logger::INFO
